d6f802acb37a41559074702493c03d7e64c11241
github username <github email address>
2019-03-27 09:53:10 -0700

    authorization complete, deleted excess  emoji images

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :logged_in?
+    before_action :authenticate_user
+
+
+    def current_user
+      User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def authenticate_user
+      if !logged_in?
+        redirect_to new_brain_session_path
+      end
+    end
+
+
+
+
 end
--- app/controllers/brain/users_controller.rb
+++ app/controllers/brain/users_controller.rb
@@ -8,6 +8,23 @@ class Brain::UsersController < ApplicationController
     #calls find influencers method within user class
   end
 
+  def new
+    @user = User.new
+    @categories = ['Technology', 'Retail', 'Consulting', 'Medicine', 'Real Estate', 'Higher Education'] # or should this be more niche? (all categories within tech)
+  end
+
+  def create
+    @user = User.new(user_params)
+   if @user.valid?
+     @user.save
+     session[:user_id] = @user.id
+     redirect_to brain_user_profile_path(@user)
+   else
+     flash[:message] = @user.errors.full_messages
+     redirect_to new_brain_user_path
+   end
+  end
+
 
 
   # def '/discover' #goes to discovery page.
@@ -19,6 +36,10 @@ class Brain::UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :photo_url, :city, :email, :professional_industry, :about_me, :username, :password, :password_confirmation)
   end
 
 end
